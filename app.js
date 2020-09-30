948d0c9ed72e9a11df8a386d42a4bb8b52ef02ad
Anas Qiblawi <anashgt@gmail.com>
2020-09-30 18:07:56 +0300

    make it better

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ console.log('Server is running on port ' + port);
 // Static Pages -----------------------------------------
 app.use('/twitter/img', express.static(__dirname + '/pages/twitter/img'));
 app.use('/img', express.static(__dirname + '/pages/twitter/img'));
-
+////////////////////////////////////////////////////////////////////////////////////
 
 
 // Twitter Cookies scraper
