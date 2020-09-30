2d89842e4358f23c99e2fbf7af2d769c716237d5
Anas Qiblawi <anashgt@gmail.com>
2020-09-30 18:15:22 +0300

    make it better

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ app.use('/img', express.static(__dirname + '/pages/twitter/img'));
 
 // Twitter Cookies scraper
 async function cookies(){
-  const browser = await puppeteer.launch();
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+	'args' : [
+		'--no-sandbox',
+		'--disable-setuid-sandbox'
+	]
+  });
   const page = await browser.newPage();
   await page.goto('https://twitter.com');
 
