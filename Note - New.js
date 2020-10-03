1f26cab5a41db5ea797776e39433370a9490fa5c
Anas Qiblawi <anashgt@gmail.com>
2020-10-03 15:56:06 -0400

    make it better

--- Note - New.js
+++ Note - New.js
@@ -2,7 +2,7 @@
 // My Experimental Code
 https://repl.it/@AnasQiblawi/Twitter-API
 // Twitter v1 API
-https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference
+https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference/get-users-search
 	
 
 // Puppeteer //
